33e4fadbe459033990130c9a279e9f6b6509f29c
deardyyy <deardy1603@gmail.com>
2025-04-25 00:40:02 -0500

    Update HistorialClinico.java

--- src/main/java/pe/edu/upc/medih/entities/HistorialClinico.java
+++ src/main/java/pe/edu/upc/medih/entities/HistorialClinico.java
@@ -4,24 +4,27 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "HistorialClinico")
+@Table(name = "historial_clinico")
 public class HistorialClinico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_historial")
     private int idHistorial;
 
-    @Column(name = "fecha_registro", columnDefinition = "DATETIME", nullable = false)
+    @Column(name = "fecha_registro", nullable = false)
     private LocalDateTime fechaRegistro;
 
     @ManyToOne
     @JoinColumn(name = "id_usuario", nullable = false)
     private Usuario usuario;
 
+    // Constructor vacío
     public HistorialClinico() {
-        this.fechaRegistro = LocalDateTime.now();
+        this.fechaRegistro = LocalDateTime.now(); // Asigna fecha automática por defecto
     }
 
+    // Getters y Setters
     public int getIdHistorial() {
         return idHistorial;
     }

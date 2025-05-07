5d617d4f118f2d7c6ccac1bce6425989fcd02815
stephano-2002 <95435779+stephano-2002@users.noreply.github.com>
2025-05-07 12:23:42 -0500

    query historial clinico segun ID

--- src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
+++ src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
@@ -1,8 +1,14 @@
 package pe.edu.upc.medih.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.medih.entities.HistorialClinico;
 
+import java.util.List;
+
 public interface IHistorialClinicoRepository extends JpaRepository<HistorialClinico, Integer> {
+       // obitene todos los historiales cloiinicos de un paciente segun id
+    @Query("SELECT h FROM HistorialClinico h WHERE h.paciente.id = :pacienteId")
+    List<HistorialClinico> findByPacienteId(@Param("pacienteId") Integer pacienteId);
 }

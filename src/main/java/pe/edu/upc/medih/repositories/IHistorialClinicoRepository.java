91a033937147fb6cc9e8d9774e62bc5665c0511b
deardyyy <deardy1603@gmail.com>
2025-05-07 10:39:47 -0500

    Update IHistorialClinicoRepository.java

--- src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
+++ src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.upc.medih.entities.HistorialClinico;
 
 public interface IHistorialClinicoRepository extends JpaRepository<HistorialClinico, Integer> {
+    @Query
 }

8910bb484d43baf3d27e6eed98c2d8313038abd9
deardyyy <deardy1603@gmail.com>
2025-05-09 01:19:15 -0500

    Update IHistorialClinicoRepository.java
    QUERY UPDATE HISTORIAL CLINICO

--- src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
+++ src/main/java/pe/edu/upc/medih/repositories/IHistorialClinicoRepository.java
@@ -1,9 +1,12 @@
 package pe.edu.upc.medih.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pe.edu.upc.medih.entities.HistorialClinico;
+import pe.edu.upc.medih.entities.Usuario;
 
 import java.util.List;
 
@@ -11,4 +14,9 @@ public interface IHistorialClinicoRepository extends JpaRepository<HistorialClin
        // obitene todos los historiales cloiinicos de un paciente segun id
     @Query(value = "SELECT * FROM historial_clinico h WHERE id_usuario = :pacienteId",nativeQuery = true)
     List<HistorialClinico> findByPacienteId(@Param("pacienteId") Integer pacienteId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE HistorialClinico h SET h.usuario = :nuevoUsuario WHERE h.usuario.id = :idPaciente")
+    void reasignarHistoriales(@Param("nuevoUsuario") Usuario nuevoUsuario, @Param("idPaciente") long idPaciente);
+
 }
